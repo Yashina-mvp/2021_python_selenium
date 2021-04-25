95bcf850d78f3f485704e8d9b9a38f789ae6fdf1
Yashina <ygaeta@anexinet.com>
2021-04-24 20:07:18 -0500

    enter the commit message for your changes. Lines starting

---
+++ Module 01/Selenium_hello_world.py
@@ -0,0 +1,10 @@
+"""First Selenium example"""
+
+from selenium import webdriver
+
+driver_path='/Users/yashg/2021_python_selenium/drivers/chromedriver'
+driver = webdriver.Chrome(executable_path=driver_path)
+driver.get('https://google.com')
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('QA minds')
+driver.quit()
